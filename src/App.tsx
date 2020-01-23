801c61b5978fc2838d953eda5d94f40bb1a68214
UgwuokeSundayErnest77 <sundayernestu615@gmail.com>
2020-01-23 09:14:45 -0500

    update on react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export const App = () => {
+  return(
+    <div className= {'my-app'}>
+      <h1>Hello world</h1>
+      <p>This is my new begining</p>
+      <p>The things i will like to learn are...</p>
+      <ul>
+        <li>state in react</li>
+        <li>forms</li>
+        <li>Styling</li>
+        <li>props</li>
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+
+function App2() {
+  return(
+    <div className= {'text'}>
+      <h1>Hello world</h1>
+      <p>This is my new begining</p>
+      <p>The things i will like to learn are...</p>
+      <ul>
+        <li>state in react</li>
+        <li>forms</li>
+        <li>Styling</li>
+        <li>props</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App2
+
+
+export class DateTimeNow extends React.Component {
+  render () {
+    const dateTimeNow= new Date().toLocaleString()
+  return <div>The current time in your state is {dateTimeNow}</div>
+  }
+}
